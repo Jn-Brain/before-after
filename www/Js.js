99d8af6f6ae49a2485dc281b558f71c0f838b13d
Jn-Brain <kato-jun619@outlook.jp>
2022-02-19 13:02:24 +0900

    修正

---
+++ www/Js.js
@@ -0,0 +1,14 @@
+// This is a JavaScript file
+            window.onload = function(){
+            var textbox_element = document.getElementById('textbox');
+            var new_element1 = document.createElement('p');
+            new_element1.className = 'text';
+            new_element1.innerHTML = "じゅげむじゅげむごこうのすりきれかいじゃりすいぎょのすいぎょうまつうんらいまつふうらいまつくうねるところにすむところやぶらこうじのぶらこうじぱいぽぱいぽぱいぽのしゅーりんがんしゅーりんがんのぐーりんだいぐーりんだいのぽんぽこぴーのぽんぽこなーのちょうきゅうめいのちょうすけじゅげむじゅげむごこうのすりきれかいじゃりすいぎょのすいぎょうまつうんらいまつふうらいまつくうねるところにすむところやぶら.......";
+            
+            var new_element3 = document.createElement('p');
+            new_element3.className = 'readmore-btn';
+            new_element3.innerHTML = "続きをよむ";
+            // 指定した要素の中の末尾に挿入
+            textbox_element.appendChild(new_element1);
+            textbox_element.appendChild(new_element2);
+            }
